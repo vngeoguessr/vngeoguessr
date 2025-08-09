09fce13e355a622f2bece39c8eca515177ad8bb7
tiennm99 <tiennm99@outlook.com>
2025-08-09 11:30:39 +0700

    feat: add vercel speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
